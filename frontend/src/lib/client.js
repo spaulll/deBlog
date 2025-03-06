5972f087b114e1624e7ba201a64444e3eba71706
lobstr <djspaul99@gmail.com>
2025-03-06 20:10:43 +0530

    fix: env value not fetching

--- frontend/src/lib/client.js
+++ frontend/src/lib/client.js
@@ -1,11 +1,7 @@
 import { createThirdwebClient } from "thirdweb";
-import dotenv from "dotenv";
-
-// initialize dotenv
-dotenv.config();
 
 // check if THIRDWEB_CLIENT_ID is defined
-const clientId = process.env.THIRDWEB_CLIENT_ID;
+const clientId = import.meta.env.VITE_THIRDWEB_CLIENT_ID;
 if (clientId === undefined) {
     throw new Error("THIRDWEB_CLIENT_ID is not defined");
 }
