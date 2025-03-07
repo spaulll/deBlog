91aec44434130096a72f5f3d53d454ceffafa4d9
lobstr <djspaul99@gmail.com>
2025-03-07 11:08:52 +0530

    feat: removed access_token;  added connect wallet btn if not logged in

--- frontend/src/pages/editor.pages.jsx
+++ frontend/src/pages/editor.pages.jsx
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../App";
+// import { UserContext } from "../App";
 import { Navigate, useParams } from "react-router-dom";
 import BlogEditor from "../components/blog-editor.component";
 import PublishEditor from "../components/publish-form.component";
 import { createContext } from "react";
 import Loader from "../components/loader.component";
 import axios from "axios";
+import ConnectButtonAuth from "../components/web3Component/ConnectButtonAuth";
+import { useAuth } from "../contexts/AuthContext";
 
 const blogStructure = {
   title: "",
@@ -21,32 +23,35 @@ export const EditoContext = createContext({});
 
 const Editor = () => {
 
-  let {blog_id} =useParams();
+  let { blog_id } = useParams();
   const [blog, setBlog] = useState(blogStructure);
   const [editorState, setEditorState] = useState("editor");
   const [textEditor, setTextEditor] = useState({ isReady: false });
-  const[loading,setLoading]=useState(true);
-  useEffect(()=>{
-    if(!blog_id){
-      return(setLoading(false))
+  const [loading, setLoading] = useState(true);
+
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    if (!blog_id) {
+      return (setLoading(false))
     }
     axios.post(import.meta.env.VITE_SERVER_URL + "/get-blog", {
       blog_id, draft: true, mode: 'edit'
     })
-    .then((response) => {
-      // Access response.data.blog instead of destructuring directly
-      setBlog(response.data.blog);
-      console.log(response.data.blog);
-      setLoading(false);
-    })
-    .catch((err) => {
-      setBlog(null);
-      setLoading(false);
-    });
-  },[])
-  let {
-    userAuth: { access_token },
-  } = useContext(UserContext);
+      .then((response) => {
+        // Access response.data.blog instead of destructuring directly
+        setBlog(response.data.blog);
+        console.log(response.data.blog);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setBlog(null);
+        setLoading(false);
+      });
+  }, [])
+  // let {
+  //   userAuth: { access_token },
+  // } = useContext(UserContext);
 
   return (
     <EditoContext.Provider
@@ -59,14 +64,19 @@ const Editor = () => {
         setTextEditor,
       }}
     >
-      {access_token === null ? (
-        <Navigate to="/signin" />
-      ) : loading? <Loader/>:
-      editorState == "editor" ? (
-        <BlogEditor />
-      ) : (
-        <PublishEditor />
-      )}
+      { isLoggedIn === false ? (
+        <div className="flex flex-col justify-center items-center h-screen">
+          <h2 className="text-3xl font-semibold">You are not logged in. Please connect your wallet</h2>
+          <div className="h-5" />
+          <ConnectButtonAuth />
+        </div>
+        
+      ) : loading ? <Loader /> :
+        editorState == "editor" ? (
+          <BlogEditor />
+        ) : (
+          <PublishEditor />
+        )}
     </EditoContext.Provider>
   );
 };
