8751e403425d5ec25b31c066385c2c9de4a5ba01
lobstr <djspaul99@gmail.com>
2025-04-22 22:51:58 +0530

    feat: search blogs by tag, keyword, username or address

--- frontend/src/pages/search.page.jsx
+++ frontend/src/pages/search.page.jsx
@@ -5,7 +5,7 @@ import Loader from "../components/loader.component";
 import AnimationWrapper from "../common/page-animation";
 import BlogPostCard from "../components/blog-post.component";
 import NoDataMessage from "../components/nodata.component";
-import LoadMoreDataBtn from "../components/load-more.component";
+// import LoadMoreDataBtn from "../components/load-more.component";
 import { filterPaginationData } from "../common/filter-pagination-data";
 import axios from "axios";
 import UserCard from "../components/usercard.component";
@@ -16,74 +16,67 @@ const SearchPage = () => {
   let { query } = useParams();
   let [blogData, setBlogData] = useState(null);
   let [users, setUsers] = useState(null);
-  const searchBlogs = ({ page = 1, create_new_arr = false }) => {
+  const searchBlogs = () => {
     axios
-      .post(import.meta.env.VITE_SERVER_URL + "/search-blogs", {
-        query,
-        page,
+      .get(import.meta.env.VITE_SERVER_URL + "/api/search-blogs", {
+        params: {
+          query,
+        },
       })
       .then(async ({ data }) => {
-        let formatData = await filterPaginationData({
-          state: blogData,
-          data: data.blogs,
-          page,
-          countRoute: "/search-blogs-count",
-          data_to_send: { query },
-          create_new_arr,
-        });
-        setBlogData(formatData);
-        // console.log(formatData)
+        console.log(data.blogs);
+        setBlogData(data.blogs);
       })
       .catch((err) => {
-        console.log(err);
+        console.error(err);
       });
   };
 
-  const fetchUsers = () => {
-    axios
-      .post(import.meta.env.VITE_SERVER_URL + "/search-users", {
-        query,
-      })
-      .then(async ({ data: { users } }) => {
-        setUsers(users);
-        // console.log(formatData)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+  // const fetchUsers = () => {
+  //   axios
+  //     .post(import.meta.env.VITE_SERVER_URL + "/search-users", {
+  //       query,
+  //     })
+  //     .then(async ({ data: { users } }) => {
+  //       setUsers(users);
+  //       // console.log(formatData)
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
 
   useEffect(() => {
     resetState();
-    searchBlogs({ page: 1, create_new_arr: true });
-    fetchUsers();
+    searchBlogs();
+    // fetchUsers();
   }, [query]);
   const resetState = () => {
     setBlogData(null);
     setUsers(null);
   };
-  const UserCardWrapper = () => {
-    return (
-      <>
-        {users == null ? (
-          <Loader />
-        ) : users.length ? (
-          users.map((user, i) => {
-            return (
-              <AnimationWrapper
-                key={i}
-                transation={{ duration: 1, delay: i * 0.08 }}
-              >
-                <UserCard user={user} />
-              </AnimationWrapper>
-            );
-          })
-        ) : (
-          <NoDataMessage message="no user found" />
-        )}
-      </>
-    );
-  };
+  // const UserCardWrapper = () => {
+  //   return (
+  //     <>
+  //       {users == null ? (
+  //         <Loader />
+  //       ) : users.length ? (
+  //         users.map((user, i) => {
+  //           return (
+  //             <AnimationWrapper
+  //               key={i}
+  //               transation={{ duration: 1, delay: i * 0.08 }}
+  //             >
+  //               <UserCard user={user} />
+  //             </AnimationWrapper>
+  //           );
+  //         })
+  //       ) : (
+  //         <NoDataMessage message="no user found" />
+  //       )}
+  //     </>
+  //   );
+  // };
   return (
     <section className="h-cover flex justify-center gap-10">
       <div className="w-full">
@@ -94,10 +87,10 @@ const SearchPage = () => {
           <>
             {blogData == null ? (
               <Loader />
-            ) : !blogData.results.length ? (
+            ) : !blogData.length ? (
               <NoDataMessage message="No blog published with this tag till now" />
             ) : (
-              blogData.results.map((blog, i) => {
+              blogData.map((blog, i) => {
                 return (
                   <AnimationWrapper
                     transition={{ duration: 1, delay: i * 0.1 }}
@@ -111,18 +104,18 @@ const SearchPage = () => {
                 );
               })
             )}
-            <LoadMoreDataBtn state={blogData} fetchDataFun={searchBlogs} />
+            {/* <LoadMoreDataBtn state={blogData} fetchDataFun={searchBlogs} /> */}
           </>
-          <UserCardWrapper />
+          {/* <UserCardWrapper /> */}
         </InPageNavigation>
       </div>
       <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-1 border-grey pl-8 pt-3 max-md:hidden gap-1">
         <div className="flex">
-        <User  className="m-1"/>
-        <h1 className="font-medium text-xl mb-8">user related to search</h1>
-        
+          <User className="m-1" />
+          <h1 className="font-medium text-xl mb-8">user related to search</h1>
+
         </div>
-        <UserCardWrapper />
+        {/* <UserCardWrapper /> */}
       </div>
     </section>
   );
