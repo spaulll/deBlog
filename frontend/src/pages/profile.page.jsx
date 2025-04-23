0e38b703ad38a85ba0079fc0d9eae3dfe89f8782
supriyopurkait <supriyopurkait7@gmail.com>
2025-04-23 20:40:58 +0530

    refactor: copy design with toster

--- frontend/src/pages/profile.page.jsx
+++ frontend/src/pages/profile.page.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import { useParams, Link } from "react-router-dom";
 import AnimationWrapper from "../common/page-animation";
 import { Check, Copy, Loader } from "lucide-react";
@@ -13,6 +14,7 @@ import BlogPostCard from "../components/blog-post.component";
 // import LoadMoreDataBtn from "../components/load-more.component";
 import PageNotFound from "./404.page";
 import { useAuth } from "../contexts/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 export const profileStructure = {
   personal_info: {
@@ -138,94 +140,99 @@ const ProfilePage = () => {
     try {
       await navigator.clipboard.writeText(tip_address);
       setCopied(true);
-      setTimeout(() => {
-        setCopied(false);
-      }, 2000);
+      toast.success("Address copied to clipboard!");
     } catch (err) {
+      toast.error("Address failed to copied!");
       console.error("Failed to copy!", err);
     }
   };
 
   return (
-    <AnimationWrapper>
-      {loading ? (
-        <div className="flex justify-center items-center min-h-[300px]">
-          <Loader className="animate-spin h-10 w-10 text-gray-500" />
-        </div>
-      ) : profile_username?.length ? (
-        <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12 ">
-          <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md-border-1 border-grey md:sticky md:top-[100px] md:py-10">
-            <img
-              src={profile_img}
-              className="w-48 h-48 rounded-full md:w-32 md:h-32 bg-grey"
-              alt="Profile"
-            />
-            <h1 className="text-2xl font-medium">@{profile_username}</h1>
-            <p className="text-xl capitalize h-6">{fullname}</p>
-            <div className="col-span-2 flex gap-4 items-center">
-              <p className="text-sm text-gray-500">Tip: {tip_address}</p>
-              <button className="pt-4" onClick={handelCopyAdderss}>
-                  {copied ? <Check size={"20px"} /> : <Copy size={"20px"} />}
-              </button>
-            </div>
-            <p>{total_posts.toLocaleString()} Blogs</p>
-            <div className="flex gap-4 mt-2">
-              {profileId === username ? (
-                <Link to="/settings/edit-profile" className="btn-light rounded-md">
-                  Edit Profile
-                </Link>
-              ) : null}
-            </div>
-            <AboutUser
-              className="max-md:hidden"
-              bio={bio}
-              social_links={social_links}
-              joinedAt={(joinedAt)}
-            />
+    <>
+      <AnimationWrapper>
+        {loading ? (
+          <div className="flex justify-center items-center min-h-[300px]">
+            <Loader className="animate-spin h-10 w-10 text-gray-500" />
           </div>
-
-          <div className="max-md:mt-12 w-full">
-            <InPageNavigation
-              routes={["blogs published", "about"]}
-              defaultHidden={["about"]}
-            >
-              <>
-                {blogs == null ? (
-                  <div className="flex justify-center py-10">
-                    <Loader className="animate-spin h-8 w-8 text-gray-500" />
-                  </div>
-                ) : !blogs.length ? (
-                  <NoDataMessage message="No blog published by this user yet" />
-                ) : (
-                  blogs.map((blog, i) => (
-                    <AnimationWrapper
-                      transition={{ duration: 1, delay: i * 0.1 }}
-                      key={blog.blog_id}
-                    >
-                      <BlogPostCard
-                        content={blog}
-                        author={blog.author.personal_info}
-                        activity={blog.activity}
-                        publishedAt={blog.publishedAt}
-                      />
-                    </AnimationWrapper>
-                  ))
-                )}
-
-                {/* <LoadMoreDataBtn state={blogs} fetchDataFun={getBlogs} /> */}
-              </>
+        ) : profile_username?.length ? (
+          <section className="h-cover md:flex flex-row-reverse items-start gap-5 min-[1100px]:gap-12 ">
+            <div className="flex flex-col max-md:items-center gap-5 min-w-[250px] md:w-[50%] md:pl-8 md-border-1 border-grey md:sticky md:top-[100px] md:py-10">
+              <img
+                src={profile_img}
+                className="w-48 h-48 rounded-full md:w-32 md:h-32 bg-grey"
+                alt="Profile"
+              />
+              <h1 className="text-2xl font-medium">@{profile_username}</h1>
+              <p className="text-xl capitalize h-6">{fullname}</p>
+              <div className="col-span-2 flex gap-4 items-center">
+                <button className="pt-4" onClick={handelCopyAdderss}>
+                  {/* {copied ? <Check size={"20px"} /> : <Copy size={"20px"} />} */}
+                  <p className="text-sm text-gray-500">Tip:{tip_address}</p>
+                </button>
+              </div>
+              <p>{total_posts.toLocaleString()} Blogs</p>
+              <div className="flex gap-4 mt-2">
+                {profileId === username ? (
+                  <Link
+                    to="/settings/edit-profile"
+                    className="btn-light rounded-md"
+                  >
+                    Edit Profile
+                  </Link>
+                ) : null}
+              </div>
               <AboutUser
+                className="max-md:hidden"
                 bio={bio}
                 social_links={social_links}
                 joinedAt={joinedAt}
               />
-            </InPageNavigation>
-          </div>
-        </section>
-      ) : (
-        <PageNotFound />
-      )}
-    </AnimationWrapper>
+            </div>
+
+            <div className="max-md:mt-12 w-full">
+              <InPageNavigation
+                routes={["blogs published", "about"]}
+                defaultHidden={["about"]}
+              >
+                <>
+                  {blogs == null ? (
+                    <div className="flex justify-center py-10">
+                      <Loader className="animate-spin h-8 w-8 text-gray-500" />
+                    </div>
+                  ) : !blogs.length ? (
+                    <NoDataMessage message="No blog published by this user yet" />
+                  ) : (
+                    blogs.map((blog, i) => (
+                      <AnimationWrapper
+                        transition={{ duration: 1, delay: i * 0.1 }}
+                        key={blog.blog_id}
+                      >
+                        <BlogPostCard
+                          content={blog}
+                          author={blog.author.personal_info}
+                          activity={blog.activity}
+                          publishedAt={blog.publishedAt}
+                        />
+                      </AnimationWrapper>
+                    ))
+                  )}
+
+                  {/* <LoadMoreDataBtn state={blogs} fetchDataFun={getBlogs} /> */}
+                </>
+                <AboutUser
+                  bio={bio}
+                  social_links={social_links}
+                  joinedAt={joinedAt}
+                />
+              </InPageNavigation>
+            </div>
+          </section>
+        ) : (
+          <PageNotFound />
+        )}
+      </AnimationWrapper>
+      <ToastContainer position="bottom-right" />
+    </>
   );
 };
 
