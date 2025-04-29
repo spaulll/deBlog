7263e3f23acfea3fbf2e565751cc28fc02ec8c11
lobstr <djspaul99@gmail.com>
2025-04-29 11:47:18 +0530

    chore: enhanced logout, clears JWT from browser cache

--- frontend/src/components/web3Component/logout.jsx
+++ frontend/src/components/web3Component/logout.jsx
@@ -1,22 +1,41 @@
 import { useDisconnect, useActiveWallet, useActiveAccount, ConnectButton } from "thirdweb/react";
 import { useAuth } from "../../contexts/AuthContext";
 import { client } from "../../lib/client";
+import { post } from "../../lib/api";
 
 function Logout() {
     const { disconnect } = useDisconnect();
     const { isLoggedIn, setIsLoggedIn, userName } = useAuth();
     const wallet = useActiveWallet();
+  
     if (!wallet) {
-        return null;
+      return null;
     }
+  
+    const handleLogout = async () => {
+      try {
+        const response = await post({
+            url: `${import.meta.env.VITE_SERVER_URL}/logout`
+        });
+        console.log('Logout response:', response);
+        if (response) {
+          setIsLoggedIn(false);
+          disconnect(wallet);
+        } else {
+          console.error('Logout failed');
+          throw new Error('Logout failed');
+        }
+      } catch (error) {
+        console.error('An error occurred during logout:', error);
+      }
+    };
+  
     return (
-        <button onClick={() => {
-            setIsLoggedIn(false); disconnect(wallet)
-        }} className="link pl-8 py-4 pr-14">Logout @{userName || "User"}
-        </button>
+      <button onClick={handleLogout} className="link pl-8 py-4 pr-10">
+        Logout @{userName || 'User'}
+      </button>
     );
-}
-
+  }
 function WalletInfo() {
     const wallet = useActiveAccount();
     if (!wallet) {
