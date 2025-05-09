580fa69861f759ab62571b04d3aaf7fd6e900ade
lobstr <djspaul99@gmail.com>
2025-05-09 20:05:05 +0530

    refactor: remove unused components and session management code

--- frontend/src/components/load-more.component.jsx
+++
@@ -1,9 +0,0 @@
-const LoadMoreDataBtn =({state,fetchDataFun, additionalparam}) =>{
-    if(state != null && state.totalDocs > state.results.length){
-        return(
-            <button onClick={()=>fetchDataFun({...additionalparam,page: state.page+1})} className="text-dark-grey p-2 px-3 hover:bg-grey/30 rounded-md flex items-center gap-2"> load More</button>
-        )
-    }
-    
-}
-export default LoadMoreDataBtn;
